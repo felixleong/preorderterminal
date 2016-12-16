5a6907cdc8e5a5ebf3abef4a737fddb60a6eecf7
Seh Hui, Leong <felixleong@gmail.com>
2016-12-16 18:12:48 +0800

    Initial commit w/ hodgepodge scripts.

---
+++ preorder.py
@@ -0,0 +1,26 @@
+import hug
+from marshmallow import fields
+
+
+@hug.get('/', output=hug.output_format.html)
+def index():
+    return open('index.html').read()
+
+
+@hug.post(input=[
+    hug.input_format.json,
+    hug.input_format.urlencoded])
+def preorder(
+        name: hug.types.text,
+        email: fields.Email(),
+        tier: hug.types.one_of([
+            'kotakcon2016-moral',
+            'kotakcon2016-50paid',
+            'kotakcon2016-paidfull']),
+        facebook=None,
+        twitter=None):
+    return {
+        'name': name,
+        'email': email,
+        'tier': tier
+    }
